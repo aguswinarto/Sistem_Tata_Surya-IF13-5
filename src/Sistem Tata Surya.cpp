7597f32e2c411663e658e1195adc830b5c8b0f60
Agus Winarto Pratama <agus.winarto.pratama@gmil.com>
2012-06-19 08:34:54 +0700

    Perubahan 2

--- src/Sistem Tata Surya.cpp
+++ src/Sistem Tata Surya.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Agus Winarto Pratama
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -10,13 +10,17 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <math.h>
+#include "kamera.c"
+#include "menu.c"
+#include "cahaya.c"
+#include "bola.c"
 #define TimerMSecs 33
 GLenum polygonMode = GL_FILL;
 
 int SpinOn = 0, SpinBreak = 1, startTime, prevTime, Velocity = 1;
 static int Mer = 0.0, Ven = 0.0, Ear = 0.0, Moo = 0.0, Moo2 = 0.0, Mar = 0.0, Jup =
 0.0, Sat = 0.0, Ura = 0.0, Nep = 0.0, Plu = 0.0;
-float xpos = 0, ypos = 0, zpos = 0, xrot = 0, yrot = 0, angle=0.0, lastx, lasty;
+
 GLfloat Shine[] = {10};
 GLfloat MooCol[] = {2.3, 2.3, 2.3};
 GLfloat Moo2Col[] = {0.93, 0.93, 0.93};
@@ -30,10 +34,10 @@ GLfloat UraCol[] ={0.40, 0.900, 1.0};
 GLfloat NepCol[] = {0.135, 0.115, 0.85};
 GLfloat PluCol[] = {0.7, 0.8, 1.00};
 
+
 void SceneSpin(int value)
 {
   int currTime = glutGet(GLUT_ELAPSED_TIME);
-
   if(SpinOn == 1)
     glutTimerFunc(TimerMSecs, SceneSpin, 0);
 
@@ -88,305 +92,172 @@ prevTime = currTime;
 void draw(void)
 {
   GLfloat position[] = { 0.0, 0.0, 1.5, 1.0 };
-
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
   glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, Shine);
-
   glPushMatrix();
-
     gluLookAt(80.0, 10.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-
     glPushMatrix();
 
 //Membuat Merkurius
       glRotated((GLdouble) Mer, 0.0, 1.0, 0.0);
-
       glTranslated(0.0, 0.0, 4.9);
-
       glRotated((GLdouble) Mer, 0.0, 1.0, 0.0);
-
       glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, MerCol);
-
       glLightModelfv(GL_LIGHT_MODEL_AMBIENT, MerCol);
-
       glutSolidSphere(0.049, 20, 20);
-
       glDisable(GL_DIFFUSE);
-
-
     glPopMatrix();
-
     glPushMatrix();
 
 //Membuat Venus
       glRotated((GLdouble) Ven, 0.0, 1.0, 0.0);
-
       glTranslated(0.1, 0.0, 5.2);
-glRotated((GLdouble) Ven - 1, 0.0, 1.0, 0.0); //Counter Rotation
-
+      glRotated((GLdouble) Ven - 1, 0.0, 1.0, 0.0); //Counter Rotation
       glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, VenCol);
-
       glLightModelfv(GL_LIGHT_MODEL_AMBIENT, VenCol);
-
       glutSolidSphere(0.12, 20, 20);
-
       glDisable(GL_DIFFUSE);
-
     glPopMatrix();
-
     glPushMatrix();
 
 //Membuat Bumi
       glRotated((GLdouble) Ear, 0.0, 1.0, 0.0);
-
       glTranslated(0.1, 0.0, 5.6);
-
       glRotated((GLdouble) Ear, 0.0, 1.0, 0.0);
-
       glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, EarCol);
-
       glLightModelfv(GL_LIGHT_MODEL_AMBIENT, EarCol);
-
       glutSolidSphere(0.13, 20, 20);
-
       glDisable(GL_DIFFUSE);
-
-      glPushMatrix();
+    glPushMatrix();
 
 //Membuat Bulan / Satelit Bumi
         glRotated((GLdouble) Moo, 0.0, 1.0, 0.0);
-
         glTranslated(0.1, 0.0, 0.1);
-
         glRotated((GLdouble) Moo, 0.0, 1.0, 0.0);
-
         glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, MooCol);
-
         glLightModelfv(GL_LIGHT_MODEL_AMBIENT, MooCol);
-
         glutSolidSphere(0.02, 20, 20);
-
         glDisable(GL_DIFFUSE);
-
-      glPopMatrix();
-
     glPopMatrix();
-
+    glPopMatrix();
     glPushMatrix();
 
 //Membuat Mars
       glRotated((GLdouble) Mar, 0.0, 1.0, 0.0);
-
       glTranslated(0.1, 0.0, 6.0);
-
       glRotated((GLdouble) Mar, 0.0, 1.0, 0.0);
-
       glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, MarCol);
-
       glLightModelfv(GL_LIGHT_MODEL_AMBIENT, MarCol);
-
       glutSolidSphere(0.067, 20, 20);
-glDisable(GL_DIFFUSE);
-
-      glPushMatrix();
+      glDisable(GL_DIFFUSE);
+   glPushMatrix();
 
 //Membuat Satelit 1 Mars
         glRotated((GLdouble) Moo, 0.0, 1.0, 0.0);
-
         glTranslated(0.0, 0.0, 0.1);
-
         glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, MooCol);
-
         glLightModelfv(GL_LIGHT_MODEL_AMBIENT, MooCol);
-
         glRotated((GLdouble) Moo, 0.0, 1.0, 0.0);
-
         glutSolidSphere(0.015, 20, 10);
-
       glPopMatrix();
-
       glPushMatrix();
 
 //Membuat Satelit 2 Mars
         glRotated((GLdouble) Moo2, 0.0, 1.0, 0.0);
-
         glTranslated(0.0, 0.0, 0.15);
-
         glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, Moo2Col);
-
         glLightModelfv(GL_LIGHT_MODEL_AMBIENT, Moo2Col);
-
         glRotated((GLdouble) Moo2, 0.0, 1.0, 0.0);
-
         glutSolidSphere(0.01, 20, 10);
-      glPopMatrix();
-
     glPopMatrix();
-
+    glPopMatrix();
     glPushMatrix();
 
 //Membuat Jupiter
       glRotated((GLdouble) Jup, 0.0, 1.0, 0.0);
-
       glTranslated(0.1, 0.0, 9.7);
-
       glRotated((GLdouble) Jup, 0.0, 1.0, 0.0);
-
       glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, JupCol);
-
       glLightModelfv(GL_LIGHT_MODEL_AMBIENT, JupCol);
-
       glutSolidSphere(1.42, 20, 20);
-
       glDisable(GL_DIFFUSE);
-
     glPopMatrix();
-
     glPushMatrix();
 
 //Membuat Saturnus
       glRotated((GLdouble) Sat, 0.0, 1.0, 0.0);
-
       glTranslated(0.1, 0.0, 14.0);
-
       glRotated((GLdouble) Sat, 0.0, 1.0, 0.0);
-glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, SatCol);
-
+      glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, SatCol);
       glLightModelfv(GL_LIGHT_MODEL_AMBIENT, SatCol);
-
       glutSolidSphere(1.20, 20, 20);
-
       glDisable(GL_DIFFUSE);
-
     glPopMatrix();
-
     glPushMatrix();
-
 
 //Membuat Uranus
       glRotated((GLdouble) Ura, 0.0, 1.0, 0.0);
-
       glTranslated(0.1, 0.0, 23.5);
-
-      glRotated((GLdouble) Ura - 1, 1.0, 0.1, 0.0); //Counter rotation
-                  //top-to-bottom rotation
+      glRotated((GLdouble) Ura - 1, 1.0, 0.1, 0.0);
       glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, UraCol);
-
       glLightModelfv(GL_LIGHT_MODEL_AMBIENT, UraCol);
-
       glutSolidSphere(0.51, 20, 20);
-
       glDisable(GL_DIFFUSE);
-
     glPopMatrix();
-
     glPushMatrix();
 
 
 //Membuat Neptunus
       glRotated((GLdouble) Nep, 0.0, 1.0, 0.0);
-
       glTranslated(0.1, 0.0, 34.5);
-
       glRotated((GLdouble) Nep, 0.0, 1.0, 0.0);
-
       glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, NepCol);
-
       glLightModelfv(GL_LIGHT_MODEL_AMBIENT, NepCol);
-
       glutSolidSphere(0.49, 20, 20);
-
       glDisable(GL_DIFFUSE);
-
     glPopMatrix();
-
     glPushMatrix();
 
 //Membuat Pluto
       glRotated((GLdouble) Plu, 0.0, 1.0, 0.0);
-
       glTranslated(0.1, 0.0, 44.0);
-
       glRotated((GLdouble) Plu, 0.0, 1.0, 0.0);
-
       glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, PluCol);
-
       glLightModelfv(GL_LIGHT_MODEL_AMBIENT, PluCol);
-
       glutSolidSphere(0.1, 20, 20);
-
       glDisable(GL_DIFFUSE);
-
-      glPopMatrix();
+   glPopMatrix();
 
     glLightfv(GL_LIGHT0, GL_POSITION, position);
-
     GLfloat global_ambient[] = {0.5f, 0.5f, 0.5f, 1.0f}; //Emission
-
     glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient); //Emission
-
     glDisable(GL_LIGHTING);
-
     glColor3f(1.0, 1.0, 0.0);
-
     glRotated((GLdouble) Ear, 0.0, 1.0, 0.0);
-
     glutSolidSphere(4.00, 20, 20);
-
     glEnable(GL_LIGHTING);
-
     glPopMatrix();
 
 }
 
-void enable(void)
-{
 
-  glShadeModel(GL_SMOOTH);
-
-  glEnable(GL_LIGHTING);
-
-  glEnable(GL_LIGHT0);
-
-  glEnable(GL_DEPTH_TEST);
-}
-
-void camera(void)
-{
- glRotatef(xrot,1.0,0.0,0.0);
- glRotatef(yrot,0.0,1.0,0.0);
- glTranslated(-xpos,ypos,-zpos);
- glutPostRedisplay();
-}
 
 void display(void)
 {
-  glClearColor (0.0,0.0,0.0,1.0);
+	float angle=0.0;
+
+	glClearColor (0.0,0.0,0.0,1.0);
     glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
     camera();
     enable();
     draw();
     glutSwapBuffers();
-  glPolygonMode(GL_FRONT_AND_BACK, polygonMode);
+    glPolygonMode(GL_FRONT_AND_BACK, polygonMode);
     angle++;
 }
 
-void reshape(int w, int h)
-{
-  glViewport(0, 0, (GLsizei) w, (GLsizei) h);
 
-  glMatrixMode(GL_PROJECTION);
-
-  glLoadIdentity();
-
-  gluPerspective(40.0, (GLfloat) w/(GLfloat) h, 1.0, 500.0);
-
-  glMatrixMode(GL_MODELVIEW);
-
-  glLoadIdentity();
-}
 void WireFrameMode() {
  if (polygonMode == GL_FILL ) {
   polygonMode = GL_LINE;
@@ -397,6 +268,8 @@ void WireFrameMode() {
 }
 void Keyboard(unsigned char key, int A, int B)
 {
+	float xpos = 0, ypos = 0, zpos = 0, xrot = 0, yrot = 0;
+
    if(key == 'x')
     {
     xrot += 1;
@@ -483,28 +356,12 @@ void mouseMenu(int value)
 
   if(value == 2)
     SceneSpin(2);
-
   if(value == 3)
     WireFrameMode();
-
   if(value == 4)
     exit(0);
 }
 
-void menu(void)
-{
-  printf("--------MENU--------\n");
-  printf("Tekan 1 untuk start/pause\n");
-  printf("Tekan 2 untuk meninggkatkan kecepatan\n");
-  printf("Tekan 3 untuk menurunkan kecepatan\n");
-  printf("Klik kanan untuk informasi lebih\n\n");
-  printf("-----KONTROL KAMERA-----\n");
-  printf("W A S D untuk maju mundur dan kanan kiri\n");
-  printf("x dan z untuk zoom in dan zoom out\n\n");
-  printf("-----------------------\n");
-  printf("Tekan Esc untuk keluar\n");
-  printf("-----------------------\n");
-}
 
 
 int main(int argc, char **argv)
